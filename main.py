defda2f1605949534ed14689375c39d2c97d9fc5
poi <poi@yuudachi.moe>
2019-04-02 23:05:03 +0800

    fix errors

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ import pywikibot
 
 template_homepage = "<includeonly>\n<div style=\"width:100%\">\n{{{{Panel\n|pic    = {pic1}\n|title  = {title1}\n|tag    = 文/{author1}\n|link   = {link1}\n}}}}\n</div>\n</includeonly>"
 site = pywikibot.Site('zh', 'kcwiki')
-page = pywikibot.Page(site, "Template:镇守府沙滩")
+page = pywikibot.Page(site, "Template:首页轮播")
 
-print(page.text)
-proxy = {"http": "http://home.yuudachi.moe:4411",
-         "https": "http://home.yuudachi.moe:4411"}
 
 header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 pattern = r'var msg_cdn_url\s*=\s*\"(.*)\"'
@@ -32,7 +29,7 @@ articles = [
 
 count = 0
 for i in range(0, len(articles)-1):
-    web = requests.get(articles[i]["link"], headers=header, proxies=proxy)
+    web = requests.get(articles[i]["link"], headers=header)
     soup = BeautifulSoup(web.text, 'lxml')
     js = soup.find_all("script")
     for each in js:
@@ -41,4 +38,6 @@ for i in range(0, len(articles)-1):
             articles[i]["cover"] = match.groups()[0]
 page.text = template_homepage.format(pic1=articles[0]["cover"], link1=articles[0]["link"], title1=articles[0]["title"],
                                author1=articles[0]["author"])
+
+page.save("biz update")
 
